6a80801e3e3183409fc4569a26695c229e2d78a0
JBalila <91769667+JBalila@users.noreply.github.com>
2023-06-06 09:51:10 -0400

    Hooked up ElevenLabs voice-playing and updated ChatGPT memory

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import os
+import time
 import requests
 from dotenv import load_dotenv
 from pydub import AudioSegment
-from flask import Flask, request, Response
+from flask import Flask, request, Response, send_file
+from flask_cors import CORS
 from twilio.rest import Client as TwilioClient
 from twilio.twiml.voice_response import VoiceResponse
+from elevenlabs import set_api_key, generate, save
 import openai
 
 # Import API keys from .env file
@@ -18,21 +21,29 @@ TWILIO_PHONE_NUMBER = os.getenv('TWILIO_PHONE_NUMBER')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 ELEVENLABS_API_KEY = os.getenv('ELEVENLABS_API_KEY')
 
-# Global variables
+# Global constants
 RECORDINGS_FOLDER = os.path.join(os.getcwd(), 'recordings')
 FINAL_RECORDINGS_FOLDER = os.path.join(os.getcwd(), 'final_recordings')
-RESPONSE_FOLDER = os.path.join(os.getcwd(), 'responses')
-context = 'Hey, how are you?'
+RESPONSE_FILE = os.path.join(os.getcwd(), 'static', 'response.wav')
+INITIAL_PROMPT = 'Hey... how are you?'
+VOICES = ['Rachel', 'Domi', 'Bella', 'Antoni']
+
+# Global program variables
+context = INITIAL_PROMPT
 orderNum = 1
 recordingReady = False
 recordingFilepath = None
-
+messageHistory = [
+    {'role': 'system', 'content': 'You are calling your friend. Do not say you are an AI.'},
+    {'role': 'assistant', 'content': INITIAL_PROMPT}
+]
 
 # Create a Flask web server
 app = Flask(__name__)
 
-# Configure OpenAI parameters
-openai.api_key = OPENAI_API_KEY
+# Set API Keys
+set_api_key(ELEVENLABS_API_KEY)     # For ElevenLabs
+openai.api_key = OPENAI_API_KEY     # For OpenAI
 
 # Create a call and connect it to the web server
 # This call will be used to handle the conversation
@@ -47,6 +58,13 @@ twilioClient.calls.create(
     status_callback_method='POST'
 )
 
+@app.route('/get-response-wav', methods=['GET'])
+def get_response_wav():
+    if os.path.exists(RESPONSE_FILE):
+        return send_file(RESPONSE_FILE, mimetype='audio/mpeg')
+    else:
+        return 'Nothing to send'
+
 # Use <context> to say something with ElevenLabs, then listen for response
 # After listening to response, send .wav to Whisper to transcribe it
 # After transcribing, use ChatGPT to generate natural response
@@ -57,10 +75,16 @@ def prompt():
     global context
     response = VoiceResponse()
 
-    # Create .wav file of <context> with ElevenLabs, store in 'responses/' folder
+    # Generate raw audio byte-data with ElevenLabs and save to 'responses/response.wav'
+    audioBytes = generate(
+        text=context,
+        voice='Bella',
+        model='eleven_monolingual_v1'
+    )
+    save(audioBytes, RESPONSE_FILE)
 
     # Play the <responseWAV> to the recipient and record their response
-    response.say(context)
+    response.play(f'{NGROK_ADDRESS}/get-response-wav')
     response.record(
         timeout=2,
         playBeep=False,
@@ -115,27 +139,32 @@ def upload_recording():
 @app.route('/generate-response', methods=['POST'])
 def generate_response():
     # Declare global vars
-    global context
     global recordingFilepath
+    global messageHistory
+    global context
     response = VoiceResponse()
 
     # Transcribe mp3 file with OpenAI's Whisper
     transcription = openai.Audio.transcribe('whisper-1', open(recordingFilepath, 'rb')).text
     print('Transcription: ' + transcription)
 
-    # Use <transcription> to update <context> and play it back to recipient
+    # Update the <messageHistory> with the user's response
+    messageHistory.append(
+        {'role': 'user', 'content': transcription}
+    )
+
+    # Use <transcription> to update <context>
     gptResponse = openai.ChatCompletion.create(
         model='gpt-3.5-turbo',
-        messages=[
-            {'role': 'system', 'content': 'You are calling your friend to say hello. Pretend you are not an AI.'},
-            {'role': 'user', 'content': transcription}
-        ]
+        messages=messageHistory
     )
     context = gptResponse.choices[0].message.content
     print('Context: ' + context)
 
-    # TODO: Remove after testing '/merge-wavs' endpoint
-    context = 'Test'
+    # Update the <messageHistory> with ChatGPT's response
+    messageHistory.append(
+        {'role': 'assistant', 'content': context}
+    )
 
     # Use the updated <context> to respond to the recipient
     response.redirect('/prompt', methods=['POST'])
@@ -150,7 +179,7 @@ def merge_wavs():
 
     # Nothing in 'recordings/' folder
     if (len(wavList) == 0):
-        return
+        return 'Nothing to merge'
 
     # Concatenate wav snippets into a single wav
     fullWAV = AudioSegment.empty()
@@ -165,6 +194,9 @@ def merge_wavs():
     # Remove wav files from 'recordings/ folder
     for wav in wavList:
         os.remove(os.path.join(RECORDINGS_FOLDER, wav))
+    
+    # Remove response.wav from 'static/' folder
+    os.remove(RESPONSE_FILE)
 
     return 'Done merging wavs'
 
