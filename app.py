ab4d1b15791f6b1f41a79b210f3399e3f665777f
JBalila <91769667+JBalila@users.noreply.github.com>
2023-06-08 15:20:35 -0400

    Stopped recording calls, changed transcription to be real-time

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ from globalConstants import *
 
 # Global program variables
 aiResponse = INITIAL_PROMPT
-orderNum = 1
-recordingReady = False
-recordingFilepath = None
+transcription = ''
 messageHistory = [
-    {'role': 'system', 'content': 'You are calling your friend. Do not say you are an AI.'},
+    {'role': 'system', 'content': 'Pretend you are calling your friend. Do not say you are an AI.'},
     {'role': 'assistant', 'content': INITIAL_PROMPT}
 ]
 
@@ -26,12 +24,10 @@ twilioClient.calls.create(
     to='+13059342479',
     from_=TWILIO_PHONE_NUMBER,
     url=f'{NGROK_ADDRESS}/prompt',
-    method='POST',
-    status_callback=f'{NGROK_ADDRESS}/merge-wavs',
-    status_callback_event='completed',
-    status_callback_method='POST'
+    method='POST'
 )
 
+# Returns the ElevenLabs generated response .wav file
 @app.route('/get-response-wav', methods=['GET'])
 def get_response_wav():
     if os.path.exists(RESPONSE_FILE):
@@ -45,127 +41,75 @@ def get_response_wav():
 @app.route('/prompt', methods=['POST'])
 def prompt():
     # Declare global vars
-    global orderNum
     global aiResponse
+
+    # Declare local vars
     response = VoiceResponse()
 
-    # Generate raw audio byte-data with ElevenLabs and save to 'responses/response.wav'
+    # Generate audio for <aiResponse> and play back to the recipient
     audioBytes = generate(
         text=aiResponse,
         voice='Bella',
         model='eleven_monolingual_v1'
     )
     save(audioBytes, RESPONSE_FILE)
-
-    # Play the <responseWAV> to the recipient and record their response
     response.play(f'{NGROK_ADDRESS}/get-response-wav')
-    response.record(
-        timeout=2,
-        maxLength=10,
-        playBeep=False,
+
+    # Open up a stream to listen for the response
+    # Transcribe the call as it goes, stopping when there's a pausee
+    gather = Gather(
         action=f'{NGROK_ADDRESS}/generate-response',
+        input='speech',
+        language='en-US',
         method='POST',
-        recording_status_callback=f'{NGROK_ADDRESS}/upload-recording',
-        recording_status_callback_event='completed',
-        recording_status_callback_method='GET'
+        partialResultCallback=f'{NGROK_ADDRESS}/transcribe',
+        partialResultCallbackMethod='POST',
+        profanityFilter=False,
+        speechTimeout='auto',
+        speechModel='experimental_conversations',
+        actionOnEmptyResult=True
     )
+    response.append(gather)
 
     return Response(str(response), 200, mimetype='application/xml')
 
-# Recording has ended, add to 'recordings/' folder
-@app.route('/upload-recording', methods=['GET'])
-def upload_recording():
-    global recordingFilepath
-    global recordingReady
-    global orderNum
+# Use Twilio's <Gather> verb to transcribe the call in real-time
+@app.route('/transcribe', methods=['POST'])
+def transcribe():
+    global transcription
 
-    # Get info from request body (<RecordingUrl> and <CallSid>)
-    args = request.args.to_dict()
-    recordingURL = str(args['RecordingUrl'])
-    callSID = str(args['CallSid'])
-    filename = f'{orderNum}_{callSID}.wav'
-    recordingFilepath = os.path.join(RECORDINGS_FOLDER, filename)
+    partialSpeech = request.form.get('UnstableSpeechResult')
+    if (partialSpeech):
+        transcription = partialSpeech
+        print(transcription)
+    
+    return 'Transcribing...'
 
-    # Download URL and write to 'recordings/' folder
-    wavFile = requests.get(recordingURL, allow_redirects=True)
-    open(recordingFilepath, 'wb').write(wavFile.content)
-
-    orderNum += 1
-    recordingReady = True
-    return 'Recording finished'
-
-# Use Whisper to transcribe the recording and ChatGPT-3.5 to generate a response
+# Use ChatGPT-3.5 to generate a response
 @app.route('/generate-response', methods=['POST'])
 def generate_response():
     # Declare global vars
-    global recordingFilepath
     global messageHistory
     global aiResponse
+    global transcription
 
+    # Declare local vars
     response = VoiceResponse()
-    audioFile = None
 
-    # Check if the audio file is ready to be opened
-    try:
-        audioFile = open(recordingFilepath, 'rb')
-    # <audioFile> isn't ready yet, play a random filler-phrase to fill time
-    except:
-        audioBytes = generate(
-            text=FILLER_PHRASES[random.randint(0, len(FILLER_PHRASES)-1)],
-            voice='Bella',
-            model='eleven_monolingual_v1'
-        )
-        save(audioBytes, RESPONSE_FILE)
-        response.play(f'{NGROK_ADDRESS}/get-response-wav')
-        response.redirect(f'{NGROK_ADDRESS}/generate-response')
-    # <audioFile> ready to use
-    else:
-        # Transcribe <audioFile> with OpenAI's Whisper
-        transcription = openai.Audio.transcribe('whisper-1', audioFile).text
+    # Update <messageHistory> with <transcription> and create reply with ChatGPT
+    messageHistory.append({'role': 'user', 'content': transcription})
+    gptResponse = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=messageHistory
+    )
+    aiResponse = gptResponse.choices[0].message.content
 
-        # Update <messageHistory> with <transcription> and create reply with ChatGPT
-        messageHistory.append({'role': 'user', 'content': transcription})
-        gptResponse = openai.ChatCompletion.create(
-            model='gpt-3.5-turbo',
-            messages=messageHistory
-        )
-        aiResponse = gptResponse.choices[0].message.content
+    # Update the <messageHistory> with ChatGPT's response
+    messageHistory.append({'role': 'assistant', 'content': aiResponse})
+    transcription = ''
 
-        # Update the <messageHistory> with ChatGPT's response
-        messageHistory.append({'role': 'assistant', 'content': aiResponse})
-        response.redirect('/prompt', methods=['POST'])
-    
+    response.redirect('/prompt', methods=['POST'])
     return Response(str(response), 200, mimetype='application/xml')
-
-# Call has ended, merge .wav files into one .wav file
-@app.route('/merge-wavs', methods=['POST'])
-def merge_wavs():
-    # Get list of all wavs
-    wavList = os.listdir(RECORDINGS_FOLDER)
-    wavList.sort(key = lambda wavFile: int(wavFile.split('_')[0]))
-
-    # Nothing in 'recordings/' folder
-    if (len(wavList) == 0):
-        return 'Nothing to merge'
-
-    # Concatenate wav snippets into a single wav
-    fullWAV = AudioSegment.empty()
-    for wav in wavList:
-        wavPath = os.path.join(RECORDINGS_FOLDER, wav)
-        fullWAV += AudioSegment.from_file(wavPath, format='wav')
-
-    # Save <fullWav> into 'final_recordings/' folder
-    fullWAVName = wavList[0].split('_')[1]
-    fullWAV.export(os.path.join(FINAL_RECORDINGS_FOLDER, fullWAVName), format='wav')
-
-    # Remove wav files from 'recordings/ folder
-    for wav in wavList:
-        os.remove(os.path.join(RECORDINGS_FOLDER, wav))
-    
-    # Remove response.wav from 'static/' folder
-    os.remove(RESPONSE_FILE)
-
-    return 'Done merging wavs'
 
 # Run the app
 if __name__ == "__main__":
--- libraryImports.py
+++ libraryImports.py
@@ -5,6 +5,6 @@ import random
 from pydub import AudioSegment
 from flask import Flask, request, Response, send_file
 from twilio.rest import Client as TwilioClient
-from twilio.twiml.voice_response import VoiceResponse
+from twilio.twiml.voice_response import VoiceResponse, Gather
 from elevenlabs import set_api_key, generate, save
 import openai
